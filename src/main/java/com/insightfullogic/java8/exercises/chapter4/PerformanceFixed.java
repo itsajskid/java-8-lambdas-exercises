767da2c99189383595c3884dcf97e7e8b36cb199
Allan Shoulders <allan.shoulders@gmail.com>
2015-05-25 22:14:04 -0400

    solution for chapter 4 question 1

--- src/main/java/com/insightfullogic/java8/exercises/chapter4/PerformanceFixed.java
+++ src/main/java/com/insightfullogic/java8/exercises/chapter4/PerformanceFixed.java
@@ -1,11 +1,8 @@
 package com.insightfullogic.java8.exercises.chapter4;
 
-import com.insightfullogic.java8.examples.chapter1.Artist;
-import com.insightfullogic.java8.exercises.Exercises;
-
 import java.util.stream.Stream;
 
-import static java.util.stream.Stream.concat;
+import com.insightfullogic.java8.examples.chapter1.Artist;
 
 /** A Performance by some musicians - eg an Album or Gig. */
 public interface PerformanceFixed {
@@ -15,7 +12,11 @@ public interface PerformanceFixed {
     public Stream<Artist> getMusicians();
 
     public default Stream<Artist> getAllMusicians() {
-        return Exercises.replaceThisWithSolution();
+//        return Exercises.replaceThisWithSolution();
+
+    	return Stream.concat(getMusicians(), 
+    			getMusicians().filter(artist -> !artist.isSolo())
+    				.flatMap(artist -> artist.getMembers()));    	
     }
 
 }
